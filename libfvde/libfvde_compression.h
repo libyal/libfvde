e71c4572aa8741209385400747750c8ba1c5483e
Joachim Metz <joachim.metz@gmail.com>
2016-06-09 21:23:07 +0200

    Added compression support.

---
+++ libfvde/libfvde_compression.h
@@ -0,0 +1,47 @@
+/*
+ * Compression handling functions
+ *
+ * Copyright (C) 2009-2016, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined( _LIBFVDE_COMPRESS_H )
+#define _LIBFVDE_COMPRESS_H
+
+#include <common.h>
+#include <types.h>
+
+#include "libfvde_libcerror.h"
+
+#if defined( __cplusplus )
+extern "C" {
+#endif
+
+int libfvde_decompress_data(
+     const uint8_t *compressed_data,
+     size_t compressed_data_size,
+     uint16_t compression_method,
+     uint8_t *uncompressed_data,
+     size_t *uncompressed_data_size,
+     libcerror_error_t **error );
+
+#if defined( __cplusplus )
+}
+#endif
+
+#endif
+
