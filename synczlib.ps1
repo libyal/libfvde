f4332499dc9dd9bd4158d40b13963ff905d1d774
Joachim Metz <joachim.metz@gmail.com>
2022-11-26 03:08:02 +0100

    Changes for CI tests

--- synczlib.ps1
+++ synczlib.ps1
@@ -1,6 +1,6 @@
 # Script that synchronizes zlib.
 #
-# Version: 20220415
+# Version: 20221022
 
 Function ExtractZip($Filename)
 {
@@ -27,9 +27,9 @@ Function ExtractZip($Filename)
 	}
 }
 
-$Filename = "${pwd}\zlib1212.zip"
-$Url = "http://zlib.net/zlib1212.zip"
-$ExtractedPath = "zlib-1.2.12"
+$Filename = "${pwd}\zlib1213.zip"
+$Url = "http://zlib.net/zlib1213.zip"
+$ExtractedPath = "zlib-1.2.13"
 $DestinationPath = "..\zlib"
 
 If (Test-Path ${Filename})
